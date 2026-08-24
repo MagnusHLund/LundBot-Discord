d28de0222d2774a7f3d4ceb62314812649cad608
Magnus Lund <magnusherringelund@gmail.com>
2026-08-24 12:04:03 +0200

    Removed 2nd DSharpPlus directory

--- bot/dsharpplus/DSharpPlus/EventArgs/Ratelimit/RatelimitMetadata.cs
+++
@@ -1,6 +0,0 @@
-namespace DSharpPlus.EventArgs;
-
-/// <summary>
-/// Represents metadata sent with <see cref="RatelimitedEventArgs"/>. The exact type depends on <see cref="RatelimitedEventArgs.Opcode"/>.
-/// </summary>
-public abstract class RatelimitMetadata;
--- bot/dsharpplus/DSharpPlus/Exceptions/InvalidGatewayStateException.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace DSharpPlus.Exceptions;
-
-/// <summary>
-/// Indicates that the gateway has entered an invalid state and must reconnect.
-/// </summary>
-public sealed class InvalidGatewayStateException(string message) : Exception(message);
