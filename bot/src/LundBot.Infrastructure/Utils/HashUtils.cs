a3cbd6dd32af9cc7b39b35ca69b2ab03bfa7bb02
Magnus Lund <magnusherringelund@gmail.com>
2026-09-06 12:26:38 +0200

    Created all util classes

--- bot/src/LundBot.Infrastructure/Utils/HashUtils.cs
+++ bot/src/LundBot.Infrastructure/Utils/HashUtils.cs
@@ -1,4 +1,13 @@
+using System.Security.Cryptography;
+using System.Text;
+
 namespace LundBot.Infrastructure.Utils
 {
-    public static class HashUtils { }
+    public static class HashUtils
+    {
+        public static byte[] HashString(string input)
+        {
+            return SHA256.HashData(Encoding.UTF8.GetBytes(input));
+        }
+    }
 }
